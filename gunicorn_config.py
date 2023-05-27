e4265c159cfbc5b970578608c5c972b6b2b4a64b
Prasanth Sasikumar <prasanth.sasikumar.psk@gmail.com>
2023-05-28 02:30:57 +0800

    error message

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,3 +1,4 @@
 bind = '0.0.0.0:8000'  # The address and port Gunicorn should bind to
 workers = 4  # The number of worker processes to spawn
 module = 'viewApp:app'  # Replace with the module and application object of your web app
+errorlog = '/home/ubuntu/Website-Scrapper/gunicorn_error.log'
