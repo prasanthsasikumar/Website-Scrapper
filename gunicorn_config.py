ff8730ae7a70b93f0cc8bf16359d555d89fae89b
Prasanth Sasikumar <prasanth.sasikumar.psk@gmail.com>
2023-05-28 00:47:15 +0800

    update gunicorn

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,3 +1,3 @@
 bind = '0.0.0.0:8000'  # The address and port Gunicorn should bind to
 workers = 4  # The number of worker processes to spawn
-module = 'app:app'  # Replace with the module and application object of your web app
+module = 'viewApp:app'  # Replace with the module and application object of your web app
